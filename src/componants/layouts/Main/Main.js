0b7e2459f4a44e6440590d92b942c7693a7d7081
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-03-23 10:57:40 +0600

    layouts added

---
+++ src/componants/layouts/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BookASchedule from '../../../pages/BookASchedule/BookASchedule';
+const Main = () => {
+    return (
+        <main>
+            <BookASchedule />
+        </main>
+    );
+};
+
+export default Main;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../../componants/layouts/Footer/Footer';
+import Header from '../../componants/layouts/Header/Header';
+import Main from '../../componants/layouts/Main/Main';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
