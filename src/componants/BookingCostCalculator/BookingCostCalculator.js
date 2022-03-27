5e99ad5d00e591f8512dcfe7ac1e7f2bad932ae3
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-03-27 15:48:41 +0600

    booking process updated

--- src/componants/BookingCostCalculator/BookingCostCalculator.js
+++ src/componants/BookingCostCalculator/BookingCostCalculator.js
@@ -6,17 +6,25 @@ const BookingCostCalculator = () => {
             <div className="box shadow-sm p-2">
                 <h3 className='text-center text-dark'>Estimated Distance & Cost</h3>
                 <div className='m-3'>
-                    <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/marine-gps/">navigation gps</a></iframe>
-                    <h6 className='d-block'>Distance: 0.00 CHF</h6>
-                    <h6 className='d-block'>Cost: 0.00 CHF</h6>
+                    <iframe className='rounded shadow mb-3' width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/marine-gps/">navigation gps</a></iframe>
+                    <h6 className='d-block'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-map" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8l3-4z" />
+                        <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z" />
+                    </svg> <strong>Distance:</strong> 0.00 CHF</h6>
+                    <h6 className='d-block'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check" viewBox="0 0 16 16">
+                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                        <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                    </svg> <strong>Pessenger:</strong> 0</h6>
                     <hr />
                     <h5 className='d-block'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16">
                         <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
                         <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z" />
-                    </svg> Total Cost: 0.00 CHF</h5>
+                    </svg> <strong>Total Cost:</strong> 0.00 CHF</h5>
 
-                    <button className='btn btn-outline-danger me-2'>Back</button>
-                    <button className='btn btn-primary'>Next</button>
+                    <div className='ms-auto'>
+                        <button className='btn btn-outline-danger me-2 mt-3'>Reset</button>
+                        <button className='btn btn-outline-dark mt-3'>Find me on map</button>
+                    </div>
                 </div>
             </div>
         </section>
--- src/componants/BookingProcess/BookingProcess.js
+++ src/componants/BookingProcess/BookingProcess.js
@@ -3,11 +3,12 @@ import BookingForm from '../layouts/BookingForm/BookingForm';
 
 const BookingProcess = () => {
 
+    const [step, setStep] = useState(0)
     const [destinations, setDestination] = useState([{
         id: 1,
         name: "start_address",
         type: "text",
-        placeholder: "Start address",
+        placeholder: "Pickup Location",
         errorMessage:
             "Please enter the address where you want to get pickedup from.",
         label: "Start address",
@@ -26,68 +27,160 @@ const BookingProcess = () => {
     }])
 
 
-    const forms = [{
-        id: 1,
-        title: "Select Locations",
-        description: "Please select whare do you want to get picked up from",
-        inputs: destinations,
-        addInput: setDestination,
-        values: {
-            start_address: "",
-            destination_01: "",
-            destination_02: "",
-            destination_03: "",
-            destination_04: "",
-            destination_05: "",
-        },
-        button_display: true
-    },
-    {
-        id: 2,
-        title: "Guest Information",
-        inputs: [
+    const forms = [
+        [
             {
                 id: 1,
-                name: "total_people",
-                type: "number",
-                placeholder: "Number of People",
-                errorMessage:
-                    "Please enter the destination you want to go.",
-                label: "People",
-                required: true,
+                title: "Select Locations",
+                description: "Please select whare do you want to get picked up from",
+                inputs: destinations,
+                addInput: setDestination,
+                values: {
+                    start_address: "",
+                    destination_01: "",
+                    destination_02: "",
+                    destination_03: "",
+                    destination_04: "",
+                    destination_05: "",
+                },
+                button_display: true
             },
             {
-                id: 1,
-                name: "luggage_weight ",
-                type: "text",
-                placeholder: "Luggage Weight ",
-                errorMessage:
-                    "Please enter the destination you want to go.",
-                label: "Luggage Weight",
-                required: true,
+                id: 2,
+                title: "Trip Information",
+                inputs: [
+                    {
+                        id: 1,
+                        name: "time_pickup",
+                        type: "time",
+                        placeholder: "Time We Pick You",
+                        label: "Time We Pick You",
+                    },
+                    {
+                        id: 2,
+                        name: "date_pickup",
+                        type: "date",
+                        placeholder: "Date We Pick You",
+                        label: "Date We Pick You",
+                    },
+                    {
+                        id: 3,
+                        name: "flight_number",
+                        type: "text",
+                        placeholder: "Your Flight Number",
+                        errorMessage:
+                            "Please enter your first name!",
+                        label: "Flight Number",
+                        pattern: "^[A-Za-z0-9]{3,16}$",
+                        required: true,
+                    },
+                    {
+                        id: 4,
+                        name: "total_people",
+                        type: "number",
+                        placeholder: "Number of People",
+                        errorMessage:
+                            "Please enter the destination you want to go.",
+                        label: "People",
+                        required: true,
+                    },
+                    {
+                        id: 5,
+                        name: "luggage_weight",
+                        type: "text",
+                        placeholder: "Luggage Weight ",
+                        errorMessage:
+                            "Please enter the destination you want to go.",
+                        label: "Luggage Weight",
+                        required: true,
+                    }
+                ],
+                values: {
+                    time_pickup: "",
+                    date_pickup: "",
+                    flight_number: "",
+                    total_people: "",
+                    luggage_weight: "",
+                }
             }
-        ],
-        values: {
-            total_people: "",
-            input_2: "",
-            input_3: "",
-            input_4: "",
-            input_5: "",
-        }
-    }
-    ]
+        ]
+        ,
+        [
+            {
+                id: 2,
+                title: "Guest Information",
+                inputs: [
+                    {
+                        id: 1,
+                        name: "first_name",
+                        type: "text",
+                        placeholder: "Your First Name",
+                        errorMessage:
+                            "Please enter your first name!",
+                        label: "First Name",
+                        pattern: "^[A-Za-z0-9]{3,16}$",
+                        required: true,
+                    },
+                    {
+                        id: 2,
+                        name: "last_name",
+                        type: "text",
+                        placeholder: "Your Last Name",
+                        errorMessage:
+                            "Please enter your last name!",
+                        label: "Last Name",
+                        pattern: "^[A-Za-z0-9]{3,16}$",
+                        required: true,
+                    },
+                    {
+                        id: 3,
+                        name: "email",
+                        type: "email",
+                        placeholder: "Your Email Address",
+                        errorMessage: "It should be a valid email address!",
+                        label: "Email",
+                        required: true,
+                    },
+                    {
+                        id: 4,
+                        name: "phone",
+                        type: "phone",
+                        placeholder: "Your Phone Number",
+                        label: "Phone Nubmer",
+                        required: true,
+                    },
+                ],
+                values: {
+                    total_people: "",
+                    input_2: "",
+                    input_3: "",
+                    input_4: "",
+                    input_5: "",
+                }
+            }
+        ]
+    ];
 
     const handleSubmit = (e) => {
         e.preventDefault();
     };
 
-    console.log(forms[1].inputs)
+    let btnBackStyle
+    if (step === 0) {
+        btnBackStyle = {
+            display: "none"
+        }
+    } else {
+        btnBackStyle = {
+        }
+    }
     return (
         <section id='booking_from' className='p-3'>
             <form onSubmit={handleSubmit} className="box px-5 py-3 m-2 shadow-sm rounded">
+                <h2 className='text-center text-dark mb-3'>Book Your Trip</h2>
 
                 {
-                    forms.map((form) => (
+                    forms[step].map((form) => (
                         <BookingForm
                             key={form.id}
                             title={form.title}
@@ -99,9 +192,19 @@ const BookingProcess = () => {
                     ))
                 }
 
-                <button className='btn btn-outline-danger m-2 px-4'>Back</button>
-                <button className='btn btn-primary m-2 px-4'>Next</button>
+                <button onClick={() => setStep(step - 1)} style={btnBackStyle} className='btn btn-outline-danger m-2 px-4'>Back</button>
+                <button onClick={() => setStep(step + 1)} className='btn btn-primary m-2 px-4'>Next</button>
             </form>
+
+            <div class="card w-100 box px-5 py-3 m-2 mt-4 shadow-sm border-0 rounded text-white bg-primary">
+                <div class="card-body">
+                    <h5 class="card-title">Need Help?</h5>
+                    <p class="card-text">We are here to help you. Please call us at +41 78 676 63 69.</p>
+                    <a href="tel:41786766369" class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                    </svg> Call Now</a>
+                </div>
+            </div>
         </section>
     );
 };
