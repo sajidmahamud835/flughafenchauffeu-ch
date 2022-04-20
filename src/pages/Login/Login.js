371b0e4cf99becde97b09d554d91f8bdc082603d
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-04-20 16:38:12 +0600

    admin login created

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handelEmailChange = (e) => setEmail(e.target.value);
+    const handelPasswordChange = (e) => setPassword(e.target.value);
+
+    const handelLogin = (e) => {
+        e.preventDefault();
+        console.log(email, password)
+
+    }
+
     return (
         <div className='container w-50'>
             <form className='m-5 p-3 shadow'>
                 <h2 className='text-center pb-3'>Login</h2>
                 <div className="form-outline mb-4">
                     <label className="form-label" for="user_email">Email address</label>
-                    <input type="email" id="user_email" className="form-control" />
+                    <input onChange={handelEmailChange} type="email" id="user_email" className="form-control" />
                 </div>
 
                 <div className="form-outline mb-4">
                     <label className="form-label" for="user_pass">Password</label>
-                    <input type="password" id="user_pass" className="form-control" />
+                    <input onChange={handelPasswordChange} type="password" id="user_pass" className="form-control" />
                 </div>
 
 
@@ -26,7 +39,7 @@ const Login = () => {
                 </div>
 
 
-                <button type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
+                <button onClick={handelLogin} type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
             </form>
         </div>
     );
