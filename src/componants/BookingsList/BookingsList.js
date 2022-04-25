c60eb05711ef224d666c525ca25e54c6305af271
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-04-26 02:59:04 +0600

    admin view all bookings created

---
+++ src/componants/BookingsList/BookingsList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './BookingsList.css';
+
+const BookingsList = (props) => {
+    const { start_address, destination_01, destination_02, destination_03, destination_04, destination_05, time_pickup, date_pickup, flight_number, total_people, luggage_weight, first_name, last_name, address, city, postal_code, country, phone, email, _id, status } = props.booking || {}
+
+    const showEmail = () => alert('Email Address: ' + email);
+    const showPhone = () => alert('Phone Number: ' + phone);
+
+    return (
+
+        <tr id='booking-list'>
+            <th scope="row">{first_name} {last_name}</th>
+            <td>{start_address}</td>
+            <td>{destination_01}</td>
+            <td>{destination_02}</td>
+            <td>{destination_03}</td>
+            <td>{destination_04}</td>
+            <td>{destination_05}</td>
+            <td>{time_pickup}</td>
+            <td>{date_pickup}</td>
+            <td>{flight_number}</td>
+            <td>{total_people}</td>
+            <td>{luggage_weight}</td>
+            <td> <button onClick={showEmail} className="btn btn-primary"><i class="far fa-envelope"></i></button> </td>
+            <td><button onClick={showPhone} className="btn btn-success"><i class="fas fa-phone"></i></button></td>
+            <td>{_id}</td>
+            <td><select value={status} onChange={(e) => props.handleStatus(props.booking._id, e.target.value)} name="status" id="status">
+                <option value="Pending">Pending</option>
+                <option value="Accepted">Accepted</option>
+                <option value="Rejected">Rejected</option>
+                <option value="Canceled">Canceled</option>
+            </select></td>
+            <td>
+                <button onClick={() => props.handleDelete(props.booking._id)} type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+            </td>
+        </tr>
+
+    );
+};
+
+export default BookingsList;
--- src/pages/admin/AllBookings/AllBookings.js
+++ src/pages/admin/AllBookings/AllBookings.js
@@ -1,18 +1,25 @@
 import { getAuth } from 'firebase/auth';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './AllBookings.css'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import BookingsList from '../../../componants/BookingsList/BookingsList';
 import FirebaseApp from '../../../firebase/FirebaseApp';
+
+
 
 const AllBookings = () => {
     const auth = getAuth(FirebaseApp);
     const [user, loading, error] = useAuthState(auth);
+    const [dataChange, setDataChange] = useState([])
     let navigate = useNavigate();
+
+
 
     useEffect(() => {
         if (loading) {
-            console.log('Page is loading')
+            console.log('Page is loading.....')
         } else {
             if (!user) {
                 toast("Please login!");
@@ -22,10 +29,91 @@ const AllBookings = () => {
                 console.log(user.email);
             }
         }
-    });
+    }, [loading, user, navigate]);
+
+    const handleDelete = id => {
+        const url = `http://localhost:5000/bookings/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setDataChange(data);
+            })
+
+    }
+
+    const handleStatus = (id, status) => {
+        // eslint-disable-next-line no-restricted-globals
+        const confirmed = confirm("Are you sure to change the booking status to: " + status + "?");
+        if (confirmed === true) {
+            const url = `http://localhost:5000/bookings/${id}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    status: { status }
+                }
+                )
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    setDataChange(data);
+                })
+        }
+
+    }
+
+    const [bookings, setBookings] = useState([])
+
+    useEffect(() =>
+        fetch('http://localhost:5000/bookings')
+            .then(res => res.json())
+            .then(data => setBookings(data.bookings))
+        , [dataChange])
+
     return (
-        <div>
-            <h1>All Bookings</h1>
+        <div id='all-bookings'>
+            <div class="container">
+                <h3 className="text-center">All Bookings</h3>
+                <table class=" table  table-bordered table-sm" cellspacing="0"
+                    width="100%">
+                    <thead>
+                        <tr>
+                            <th scope="col">Client Name</th>
+                            <th scope="col">Start Address</th>
+                            <th scope="col">Dest 01</th>
+                            <th scope="col">Dest 02</th>
+                            <th scope="col">Dest 03</th>
+                            <th scope="col">Dest 04</th>
+                            <th scope="col">Dest 05</th>
+                            <th scope="col">Pickup Time</th>
+                            <th scope="col">Pickup Date</th>
+                            <th scope="col">Flight Number</th>
+                            <th scope="col">Total People</th>
+                            <th scope="col">Luggage Weight</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Booking ID</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            bookings.map((booking => <BookingsList key={booking._id} booking={booking} handleDelete={handleDelete} handleStatus={handleStatus}>
+
+                            </BookingsList>))
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
