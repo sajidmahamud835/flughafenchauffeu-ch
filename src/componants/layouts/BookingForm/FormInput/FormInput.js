97c7e233ecafb44df1b0701425493e33dfc10482
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-05-29 21:45:54 +0600

    input feild desing updated

--- src/componants/layouts/BookingForm/FormInput/FormInput.js
+++ src/componants/layouts/BookingForm/FormInput/FormInput.js
@@ -35,11 +35,27 @@ const FormInput = (props) => {
 
 
 
-                    {(showSuggestion && addressSuggestion[inputName].items.length > 0) && addressSuggestion[inputName].items.map(item => (
-                        <div className="card-body ps-3 py-1 m-1 border border-light rounded inputSugesstion" key={item.id}>
-                            <i className="fa-solid fa-location-dot"></i> <h6 onClick={(e) => onAddressChange(e, item, inputName)} className="text-primary" >{item.title}</h6>
+                    {(showSuggestion && addressSuggestion[inputName].items.length > 0) &&
+                        <div>
+                            {
+                                addressSuggestion[inputName].items.map(item => (
+                                    <div className="card-body ps-3 py-1 m-1 border border-light rounded inputSugesstion" key={item.id}>
+                                        <h6 onClick={(e) => onAddressChange(e, item, inputName)} className="" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                                        </svg> {item.title}</h6>
+                                    </div>
+
+                                ))
+                            }
+                            {
+                                <div className="card-body ps-3 py-1 m-1 border border-light rounded inputSugesstion">
+                                    <h6 onClick={(e) => onAddressChange(e, addressSuggestion[inputName].items[0], inputName)} className="" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-compass-fill" viewBox="0 0 16 16">
+                                        <path d="M15.5 8.516a7.5 7.5 0 1 1-9.462-7.24A1 1 0 0 1 7 0h2a1 1 0 0 1 .962 1.276 7.503 7.503 0 0 1 5.538 7.24zm-3.61-3.905L6.94 7.439 4.11 12.39l4.95-2.828 2.828-4.95z" />
+                                    </svg> Current Address</h6>
+                                </div>
+                            }
                         </div>
-                    ))}
+                    }
 
                 </div>}
 
