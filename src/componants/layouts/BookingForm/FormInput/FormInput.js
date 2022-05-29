1f14019943931305dc4842927b20fd2b213ec766
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-05-29 21:56:27 +0600

    calculator updated

--- src/componants/layouts/BookingForm/FormInput/FormInput.js
+++ src/componants/layouts/BookingForm/FormInput/FormInput.js
@@ -48,11 +48,11 @@ const FormInput = (props) => {
                                 ))
                             }
                             {
-                                <div className="card-body ps-3 py-1 m-1 border border-light rounded inputSugesstion">
-                                    <h6 onClick={(e) => onAddressChange(e, addressSuggestion[inputName].items[0], inputName)} className="" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-compass-fill" viewBox="0 0 16 16">
-                                        <path d="M15.5 8.516a7.5 7.5 0 1 1-9.462-7.24A1 1 0 0 1 7 0h2a1 1 0 0 1 .962 1.276 7.503 7.503 0 0 1 5.538 7.24zm-3.61-3.905L6.94 7.439 4.11 12.39l4.95-2.828 2.828-4.95z" />
-                                    </svg> Current Address</h6>
-                                </div>
+                                // <div className="card-body ps-3 py-1 m-1 border border-light rounded inputSugesstion">
+                                //     <h6 onClick={(e) => onAddressChange(e, addressSuggestion[inputName].items[0], inputName)} className="" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-compass-fill" viewBox="0 0 16 16">
+                                //         <path d="M15.5 8.516a7.5 7.5 0 1 1-9.462-7.24A1 1 0 0 1 7 0h2a1 1 0 0 1 .962 1.276 7.503 7.503 0 0 1 5.538 7.24zm-3.61-3.905L6.94 7.439 4.11 12.39l4.95-2.828 2.828-4.95z" />
+                                //     </svg> Current Address</h6>
+                                // </div>
                             }
                         </div>
                     }
