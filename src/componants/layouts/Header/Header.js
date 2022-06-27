afebf6e637df006d2b5ce9f62e89adb5f3a69b65
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-06-28 01:16:36 +0600

    website updated

--- src/componants/layouts/Header/Header.js
+++ src/componants/layouts/Header/Header.js
@@ -15,20 +15,20 @@ const Header = () => {
         <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-light ">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="//www.flughafenchauffeur.ch/"><img src="/logo.png" height="50px" alt="" /> <strong>FlughafenChauffeur</strong></Link>
+                    <Link className="navbar-brand" to="//flughafenchauffeur.ch/"><img src="/logo.png" height="50px" alt="" /> <strong>FlughafenChauffeur</strong></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="//www.flughafenchauffeur.ch/">Home</Link>
+                                <Link className="nav-link active" aria-current="page" to="//flughafenchauffeur.ch/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" href="//www.flughafenchauffeur.ch/page3.html">Sachentransporte</NavLink>
+                                <NavLink className="nav-link active" aria-current="page" href="//flughafenchauffeur.ch/page3.html">Sachentransporte</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" href="//www.flughafenchauffeur.ch/index.html#content2-11#">Kontakt</NavLink>
+                                <NavLink className="nav-link active" aria-current="page" href="//flughafenchauffeur.ch/index.html#content2-11#">Kontakt</NavLink>
                             </li>
                             {user && <li className="nav-item dropdown">
                                 <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
